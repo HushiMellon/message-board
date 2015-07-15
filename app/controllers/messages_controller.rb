e29b2f9ef0c6073745816fd990868fe3c0199421
hushimellon <hushimellon@gmail.com>
2015-07-15 11:52:37 +0000

    challenge2 repost

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -9,15 +9,15 @@ class MessagesController < ApplicationController
   def edit
     end
 
-    def update
-        if @message.update(message_params)
-            #保存に成功した場合はトップページへリダイレクト
-            redirect_to root_path , notice: 'メッセージを編集しました'
-        else
-            #保存に失敗したら編集画面へ戻す
-            render 'edit'
-        end
+  def update
+    if @message.update(message_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
     end
+  end
     
   
    ## ここから追記
